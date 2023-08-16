726766797163c65f6ebf618dfabb8c424ac2d90e
Victor Hugo <vhfn2004@gmail.com>
2023-08-16 14:53:01 -0300

    projeto iniciado

---
+++ Data/ApplicationDBCntext.cs
@@ -0,0 +1,14 @@
+﻿using Livros.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Livros.Data
+{
+    public class ApplicationDBCntext : DbContext
+    {
+        public ApplicationDBCntext(DbContextOptions<ApplicationDBCntext> options) : base(options)
+        {
+        
+        }
+        public DbSet<EmprestimoModel> Emprestimos { get; set; }
+    }
+}
---
+++ Models/EmprestimoModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Livros.Models
+{
+    public class EmprestimoModel
+    {
+        public int Id { get; set; }
+        public string Recebedor { get; set; }
+        public string Fornecedor { get; set; }
+        public string LivroEmprestado { get; set; }
+        public DateTime Receptor { get; set; } = DateTime.Now;
+    }
+}
